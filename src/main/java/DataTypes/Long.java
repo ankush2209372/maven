5facbbe58c1123dc3f9245e9436cd16ecd04da37
ankush2209372 <ankushrajput26936@gmail.com>
2026-02-03 22:01:53 +0530

    long

---
+++ src/main/java/DataTypes/Long.java
@@ -0,0 +1,22 @@
+package DataTypes;
+
+public class Long {
+    public String name;
+    public long RollNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getRollNumber() {
+        return RollNumber;
+    }
+
+    public void setRollNumber(long rollNumber) {
+        RollNumber = rollNumber;
+    }
+    }
---
+++ src/main/java/DataTypes/MainCode.java
@@ -0,0 +1,12 @@
+package DataTypes;
+
+public class MainCode {
+    public static void main(String[] args) {
+        Long Long = new Long();
+        Long.setName("ankush");
+        Long.setRollNumber(23021103001L);
+        System.out.println(Long.getName());
+        System.out.println(Long.getRollNumber());
+
+    }
+}
